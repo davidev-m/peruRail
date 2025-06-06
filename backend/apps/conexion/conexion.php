8c3f23408cf37aa94387d7a85cac1bc5f4887237
davidev-m <dmontoyaholgado@gmail.com>
2025-06-06 00:49:58 -0500

    fix:Creacion de la funcion crearConexion

--- backend/apps/conexion/conexion.php
+++ backend/apps/conexion/conexion.php
@@ -1,16 +1,19 @@
 <?php
-    //Definiendo los valores de la base de datos
-    $host = "localhost";
-    $user = "root";
-    $contraseña = "";
-    $base_datos = "perurail"; 
-
-    //Conectando
-    $conexion = new mysqli($host, $user, $contraseña, $base_datos);
+    function crearConexion(){
+        //Definiendo los valores de la base de datos
+        $host = "127.0.0.1";
+        $user = "root";
+        $contraseña = "";
+        $base_datos = "wasa"; 
+        $puerto = "3306";
     
-    //Verificacion
-    if($conexion ->connect_error){
-        die("Error". $conexion->connect_error);
+        //Conectando
+        $conexion = new mysqli($host, $user, $contraseña, $base_datos, $puerto);
+        
+        //Verificacion
+        if($conexion ->connect_error){
+            die("Error". $conexion->connect_error);
+        }
+        return $conexion;
     }
-    echo "base de datos conectado";
 ?>

ff80a03dc054aa60e8da380a42417951205b2177
David Montoya <161256573+davidev-m@users.noreply.github.com>
2025-06-08 02:40:49 -0500

    Revert "Feuture/backend formulario busqueda"

--- backend/apps/interfaz/repositorio.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    interface repositorio{
-        public function buscar($id);
-        public function guardarDatos(array $datos);
-        public function actualizar(array $datos);
-        public function eliminar(array $datos);
-    }
-
-?>

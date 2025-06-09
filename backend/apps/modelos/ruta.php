a291589d598308a961d9e6d687f85dd4c6d0eb05
davidev-m <dmontoyaholgado@gmail.com>
2025-06-09 13:40:03 -0500

    bugfix: Agregamos la creacion de nuevos metodos

---
+++ backend/apps/modelos/ruta.php
@@ -0,0 +1,70 @@
+<?php
+
+require __DIR__ . '/../conexion/conexion.php';
+require __DIR__ . '/../interfaz/interfaz_ruta.php';
+class Ruta implements interfaz_ruta{
+    private $conexion;
+    /**
+     * Consturcto de la clase Ruta
+     * Crea la conexion a la base de datos
+     */
+    public function __construct(){
+        $this->conexion = crearConexion();
+    }
+
+
+    /**
+     * Verificamos si la existencia de la ruta
+     * @param string $origen Ciudad de origen.
+     * @param string $destino Ciudad de destino.
+     * @return bool True si la ruta existe, False si no.
+     */
+    public function existe($origen, $destino){
+        $query = "SELECT * FROM Ruta WHERE Origen = ? AND Destino = ?";
+        $sentencia = $this->conexion->prepare($query);
+        $sentencia->bind_param("ss", $origen, $destino);
+        $sentencia->execute();
+        $resultado = $sentencia->get_result();
+        //Comprobamos si hay datos en la tabla
+        if($resultado->num_rows > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    /**
+     * Busca todos los destinos posibles desde un origen dado.
+     *
+     * @param string $origen Ciudad de origen.
+     * @return array Arreglo de destinos (cada uno como un array asociativo).
+     */
+    public function buscarDestino($origen){
+        $query = "SELECT Destino FROM Ruta WHERE Origen = ?";
+        $sentencia = $this->conexion->prepare($query);
+        $sentencia->bind_param('s', $origen);
+        $sentencia->execute();
+        $result = $sentencia->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC); 
+    }
+
+
+    /**
+     * Obtiene los días disponibles para una ruta específica.
+     *
+     * @param string $origen Ciudad de origen.
+     * @param string $destino Ciudad de destino.
+     * @return array|null Días disponibles como array asociativo o null si no se encuentra.
+     */
+    public function disponibilidad($origen, $destino){
+        $query = "SELECT diasDisponibles FROM Ruta WHERE origen=? AND destino =?";
+        $sentencia = $this->conexion->prepare($query);
+        $sentencia->bind_param('ss', $origen, $destino);
+        $sentencia->execute();
+        $result = $sentencia->get_result();
+        return $result->fetch_assoc();
+    }
+};
+
+?>

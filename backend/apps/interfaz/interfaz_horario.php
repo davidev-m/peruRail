3b3d86b59e9cfb511a2d7de444c8df3e68e9d819
davidev-3 <dmontoyaholgado@gmail.com>
2025-06-15 16:09:32 -0500

    feat: añadir la interfaz de viaje

---
+++ backend/apps/interfaz/interfaz_horario.php
@@ -0,0 +1,8 @@
+<?php
+    interface interfaz_horario{
+        public function buscar();
+        public function buscarHoras();
+        public function buscarFecha();
+        public function buscarDuracion();
+    };
+?>
